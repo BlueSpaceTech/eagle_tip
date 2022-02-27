4a5b2ff50c3bb72e55b02cc3a0b19733f06db839
Rakshit Kamboj <rakshitkamboj@Rakshits-MacBook-Air.local>
2022-02-27 13:03:31 +0530

    toast

--- lib/UI/views/pre_auth_screens/login_screen.dart
+++ lib/UI/views/pre_auth_screens/login_screen.dart
@@ -8,6 +8,7 @@ import 'package:eagle_tip/Utils/constants.dart';
 import 'package:eagle_tip/Utils/responsive.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -17,9 +18,40 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  FToast? fToast;
   final TextEditingController _password = TextEditingController();
   final TextEditingController _employercode = TextEditingController();
   final TextEditingController _email = TextEditingController();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fToast = FToast();
+    fToast!.init(context);
+  }
+
+  _showToast(double width) {
+    return Container(
+      alignment: Alignment.center,
+      width: Responsive.isDesktop(context) ? 300 : width * 0.8,
+      height: 40,
+      padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15.0),
+        color: Color(0xff3F4850),
+      ),
+      child: Container(
+        child: Text(
+          "This is a Custom Toast",
+          style: TextStyle(
+              color: Colors.white,
+              fontFamily: "Poppins",
+              fontWeight: FontWeight.w500),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
@@ -132,6 +164,13 @@ class _LoginScreenState extends State<LoginScreen> {
                     GestureDetector(
                       onTap: () {
                         Navigator.pushNamed(context, AppRoutes.employercode);
+                        /*
+                        fToast!.showToast(
+                          child: _showToast(width),
+                          gravity: ToastGravity.TOP,
+                          toastDuration: Duration(seconds: 3),
+                        );
+                        */
                       },
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.center,
